586c1e93934a791ed09d1c99733f146482f85ce4
felex67 <felexxx67@gmail.com>
2018-12-16 17:14:54 +0200

    qqq

--- test/error.cpp
+++
@@ -1,9 +0,0 @@
-#include "error.hpp"
-
-namespace errcodes {
-    
-}
-error::error() : errcode(errcodes::ERR_SUCCES) {}
-error::error(errcodes::code code) : errcode(code) {}
-error::~error() {}
-error::operator unsigned short() const { return (unsigned short)errcode; }
